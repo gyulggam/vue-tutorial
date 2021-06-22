3f78077f23266dcddcf3c78f6045ea1359579815
KIMJINSU <jay.kim@machbase.com>
2021-06-22 10:56:01 +0900

    tutorial init

---
+++ src/components/CreateComponents.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="createComponents">
+        <p>만들었다 컴포넌트</p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateComponents',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+    font-size: 30px;
+    color: red;
+}
+</style>
